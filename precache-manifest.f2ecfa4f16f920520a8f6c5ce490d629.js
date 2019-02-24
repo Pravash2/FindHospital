015ec633bb682882d9b49ee137c6deb5f855722e
EC2 Default User <ec2-user@ip-172-31-16-203.ec2.internal>
2019-02-24 06:52:08 +0000

    Updates

---
+++ precache-manifest.f2ecfa4f16f920520a8f6c5ce490d629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c289ae08fd302b189c2b",
+    "url": "/FindHospital/static/js/runtime~main.c289ae08.js"
+  },
+  {
+    "revision": "76d792ae0867e63d0b37",
+    "url": "/FindHospital/static/js/main.76d792ae.chunk.js"
+  },
+  {
+    "revision": "fc042165d05bac87347c",
+    "url": "/FindHospital/static/js/1.fc042165.chunk.js"
+  },
+  {
+    "revision": "76d792ae0867e63d0b37",
+    "url": "/FindHospital/static/css/main.ceb5444e.chunk.css"
+  },
+  {
+    "revision": "92ab804eb53e898a8b78a9f6356ac53f",
+    "url": "/FindHospital/index.html"
+  }
+];
